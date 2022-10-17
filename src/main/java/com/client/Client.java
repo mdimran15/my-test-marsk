0190d446c4a12eba8badcc53e87cb8cf9741f29a
mdimran15 <61236195+mdimran15@users.noreply.github.com>
2022-10-17 14:23:20 +0530

    Add files via upload

---
+++ src/main/java/com/client/Client.java
@@ -0,0 +1,9 @@
+package com.client;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("main started");
+        System.out.println("main ended");
+    }
+}
